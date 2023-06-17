b8ee5d72c650d0c53e18288b1af2869d8cbffd7e
Henrique Barbosa Sampaio <85702153+retr0lbb@users.noreply.github.com>
2023-06-17 19:31:17 -0300

    finalizado websocket

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const express = require('express');
+const WebSocket = require("ws");
+
+const app = express();
+const PORT = 6969
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server })
+
+
+wss.on("connection", (ws) =>{
+    ws.on("message", (data) =>{
+
+
+        
+
+         wss.clients.forEach(client =>{
+
+            if(client !== ws && client.readyState === WebSocket.OPEN){
+                client.send(data);
+            }
+         })
+    })
+})
+
+
+server.listen(PORT, () =>{
+    console.log(`server running on port ${PORT}`);
+})
